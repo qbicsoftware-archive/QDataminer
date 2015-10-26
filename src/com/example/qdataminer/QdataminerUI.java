778954cc8ef0c665623c84154be02f435e848a1b
segoerge <segoerge@users.noreply.github.com>
2015-10-26 15:46:01 +0100

    Maven integration for Elasticsearch Java client

--- src/com/example/qdataminer/QdataminerUI.java
+++ src/com/example/qdataminer/QdataminerUI.java
@@ -12,6 +12,12 @@ import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.settings.*;
+import org.elasticsearch.node.Node;
+import static org.elasticsearch.node.NodeBuilder.*;
+
 @SuppressWarnings("serial")
 @Theme("qdataminer")
 public class QdataminerUI extends UI {
@@ -26,6 +32,22 @@ public class QdataminerUI extends UI {
 		final VerticalLayout layout = new VerticalLayout();
 		layout.setMargin(true);
 		setContent(layout);
+		
+		//Connection to elasticsearch
+		Node node =
+			    nodeBuilder()
+			        .settings(ImmutableSettings.settingsBuilder().put("http.enabled", false))
+			        .client(true)
+			    .clusterName("qsearch")    
+			    .node();
+
+		Client client = node.client();
+		
+		GetResponse response = client.prepareGet("1000genomes_variants", "variants", "Y_14553755_C_G")
+		        .execute()
+		        .actionGet();
+		String result = response.toString();
+		layout.addComponent(new Label(result));
 
 		Button button = new Button("Click Me");
 		button.addClickListener(new Button.ClickListener() {
@@ -34,6 +56,9 @@ public class QdataminerUI extends UI {
 			}
 		});
 		layout.addComponent(button);
+		
+		//Close node when finished
+		node.close();
 	}
 
 }
