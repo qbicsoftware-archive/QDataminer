11aa3c503fcccc4e567150b1fcf759125fb05e65
segoerge <segoerge@users.noreply.github.com>
2016-01-22 11:26:48 +0100

    added license

--- src/com/qbic/qdataminer/gui/QdataminerUI.java
+++ src/com/qbic/qdataminer/gui/QdataminerUI.java
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ *     Copyright (C) 2016  Sebastian Goerges
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
 package com.qbic.qdataminer.gui;
 
 import javax.servlet.annotation.WebServlet;
@@ -12,7 +28,6 @@ import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Grid.SingleSelectionModel;
 import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.themes.Reindeer;
 import com.vaadin.ui.*;
 
 import java.util.*;
